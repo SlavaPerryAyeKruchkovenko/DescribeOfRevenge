92c486dcc6b4e0b993ee09ed6c24a050bc2c3190
Nanoster1 <danil2002.11@mail.ru>
2022-11-26 14:05:06 +0500

    Fixed conflicts

--- Character/Character.cs
+++ Character/Character.cs
@@ -2,7 +2,6 @@ using Godot;
 
 public class Character : KinematicBody2D
 {
-<<<<<<< Updated upstream
     public CharacterSystem CharSystem { get; private set; }
     public Sprite PlayerImage { get; private set; }
     private const float jumpForce = 400;
@@ -43,41 +42,4 @@ public class Character : KinematicBody2D
         GD.Print(direction);
         MoveAndSlide(direction, Vector2.Up);
     }
-=======
-	public CharacterSystem CharSystem { get; private set; }
-	public Sprite PlayerImage { get; private set; }
-	float jumpForce = 30;
-	float gravity = 9.8f;
-	public override void _Ready()
-	{
-		PlayerImage = (Sprite)GetNode("Icon");
-		CharSystem = (CharacterSystem)GetNode(nameof(CharacterSystem));
-	}
-	public override void _PhysicsProcess(float delta)
-	{
-		var direction = Vector2.Zero;
-		base._PhysicsProcess(delta);
-		if (Input.IsActionPressed("move_left"))
-		{
-			direction.x -= CharSystem.Speed;
-			PlayerImage.FlipH = true;
-		}
-		else if (Input.IsActionPressed("move_right"))
-		{
-			GD.Print(CharSystem);
-			direction.x += CharSystem.Speed;
-			PlayerImage.FlipH = false;
-		}
-		else if (Input.IsActionPressed("move_up") && IsOnFloor())
-		{
-			direction.y += jumpForce;
-		}
-		else if (Input.IsActionPressed("move_down") && IsOnFloor())
-		{
-			direction.y -= gravity;
-		}
-		MoveAndSlide(direction, Vector2.Up);
-	}
->>>>>>> Stashed changes
-
 }
